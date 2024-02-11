291b713893a089dcd8623470c8af47b4244b4615
Philip Castiglione <philipcastiglione@gmail.com>
2024-02-11 12:46:12 +0800

    added annotate gem to cms

--- weave_codex_cms/app/models/fauna.rb
+++ weave_codex_cms/app/models/fauna.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: faunas
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Fauna < ApplicationRecord
   has_rich_text :description
 
